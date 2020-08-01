d84ec39d64c23bc772e2d530afff5b9ff826567c
akshith_juego <akshith.kumar@juegostudio.net>
2020-08-01 20:17:49 +0530

    Level added

---
+++ Assets/_Assets/Script/ObstacleAnimate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class ObstacleAnimate : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    private void OnEnable()
+    {
+        GamePlayManager.GetInstance.onFinishJump += OnJumpEnd;
+        GamePlayManager.GetInstance.onStartJump += OnJumpStart;
+    }
+    private void OnDisable()
+    {
+        GamePlayManager.GetInstance.onFinishJump -= OnJumpEnd;
+        GamePlayManager.GetInstance.onStartJump -= OnJumpStart;
+    }
+    private void OnJumpStart()
+    {
+        animator.speed = 0.0f;
+    }
+    private void OnJumpEnd()
+    {
+        animator.speed = 1.0f;
+    }
+}
